ce114a1025e97570ad9203f1495f00693e641b97
Iain Walker <iain.walker@sharesight.co.nz>
2021-08-21 15:29:53 +1200

    Compass looks nice but text is wrong way up

--- src/Compass.js
+++ src/Compass.js
@@ -5,7 +5,21 @@ const Compass = props => {
   return(
     <div className="compass">
       <div className="compass__inner">
-        <div className="compass__ticker"></div>
+        <div className="compass__ticker">
+          <div>
+            { [...Array(18)].map((_key, i) => <div className="compass__mark--lg" key={`compass-lg${i + 1}`}></div>) }
+          </div>
+          <div>
+            { [...Array(18)].map((_key, i) => <div className="compass__mark--sm" key={`compass-sm${i + 1}`}></div>) }
+          </div>
+          <div>
+            {
+              ["S", "21", "24", "W", "30", "33","N", "3", "6", "E", "12", "15"].map((value, index) =>
+                <div className="compass__mark--tag" key={`compass-tag${index + 1}`}>{value}</div>
+              )
+            }
+          </div>
+        </div>
         <div className="compass_centre"></div>
       </div>
     </div>
